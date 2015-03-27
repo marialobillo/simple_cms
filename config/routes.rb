bfe2ad2018271612fe6fbe8d63cfa0f5b0ed0fe0
Neniah <maria.lobillo.santos@gmail.com>
2015-03-27 21:58:37 +0100

    subjects views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'demo/index'
-  #root "demo#index"
 
+
+  root "demo#index"
+
+  #get "demo/index"
   match ':controller(/:action(/:id))', :via => :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
