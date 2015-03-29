ceaa3fed451ae0d5716dfb840ae4c8e4e0adf396
Neniah <maria.lobillo.santos@gmail.com>
2015-03-29 20:48:29 +0200

    add new, edit

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -11,11 +11,40 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => "Default"}) 
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    
+    @subject = Subject.find(params[:id])
+
+    if @subject.update_attributes(subject_params)
+
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      render('edit')
+    end
+  
   end
 
   def delete
+  end
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "demo#index"
 
   #get "demo/index"
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
