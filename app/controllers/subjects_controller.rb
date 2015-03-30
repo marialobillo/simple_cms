ef37715044f65d47c8619c05ff4e8d7c88176d4b
Neniah <maria.lobillo.santos@gmail.com>
2015-03-30 12:17:08 +0200

    delete action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -42,6 +42,13 @@ class SubjectsController < ApplicationController
   end
 
   def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    
+    redirect_to(:action => 'index')
   end
 
   def subject_params
