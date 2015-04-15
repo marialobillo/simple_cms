9a1bdd4a01f25fba9afbe9a7f9ebedcee31bdae5
Neniah <maria.lobillo.santos@gmail.com>
2015-04-15 13:33:29 +0200

    admin users

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -12,12 +12,44 @@ class AdminUsersController < ApplicationController
 		@admin_user = AdminUser.new
 	end
 
-	def edit
+	def create
 		@admin_user = AdminUser.new(admin_user_params)
+		if @admin_user.save
+			flash[:notice] = 'Admin user created.'
+			redirect_to(:action => 'index')
+		else
+			render('new')
+	end	end
+
+	def edit
+		@admin_user = AdminUser.find(params[:id])
+	end
+
+	def update
+		@admin_user = AdminUser.find(params[:id])
+		if @admin_user.update_attributes(admin_user_params)
+			flash[:notice] = 'Admin user updated.'
+			redirect_to(:action => 'index')
+		else
+			render('edit')
+		end
 	end
 
 	def delete
-	
+		@admin_user = AdminUser.find(params[:id])
+	end
+
+	def destroy
+		AdminUser.find(params[:id]).destroy
+		flash[:notice] = 'Admin user destroyed.'
+		redirect_to(:action => 'index')
+	end
+
+	private
+
+	def amdin_user_params
+		params.require(:admin_user).permit(:first_name, :last_name, 
+			:email, :username, :password)
 	end
 
 end
