54e4b8b091d0167b7eb399f0b7959d4b88712396
Neniah <maria.lobillo.santos@gmail.com>
2015-04-21 11:46:47 +0200

    navigation menu

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,8 @@ class PublicController < ApplicationController
   
   layout 'public'
 
+  before_action :setup_navigation
+
   def index
   	# intro text
 
@@ -14,5 +16,11 @@ class PublicController < ApplicationController
   	else
 
   	end
+  end
+
+  private 
+
+  def setup_navigation
+    @subjects = Subject.visible.sorted
   end
 end
